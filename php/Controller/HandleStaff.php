2464bb2e429fc458760ab52f343680428e6bc1a9
Nguyen Hoang Linh <linh072217@gmail.com>
2021-06-01 15:02:17 +0700

    Create HandleStaff.php

---
+++ php/Controller/HandleStaff.php
@@ -0,0 +1,72 @@
+<?php
+  $rootPath = $_SERVER['DOCUMENT_ROOT'];
+  include($rootPath."/ShopPlus_Admin/php/models/Staff.php");
+  include($rootPath."/ShopPlus_Admin/php/ConnectDB.php");
+  global $connect;
+  $connect = connectDB();
+  function getStaffViaID($id){
+    $result = $GLOBALS["connect"]->query(
+      "SELECT MSNV,HOTENNV,CHUCVU,DIACHI,SODIENTHOAI FROM NHANVIEN WHERE MSNV = $id");
+    if($result->num_rows > 0){
+      $row = $result->fetch_assoc();
+      return new Staff(
+        $row["MSNV"],
+        $row['HOTENNV'],$row["CHUCVU"],
+        $row["DIACHI"],$row["SODIENTHOAI"]
+      );
+    }
+    else
+      return false;
+  }
+  function isExistStaff($id){
+    $prepare = $GLOBALS["connect"]->prepare("SELECT STAFF_LOGIN(?) AS VALID");
+    $prepare->bind_param("i",$id);
+    $prepare->execute();
+    $result = $prepare->get_result();
+    $row = $result->fetch_assoc();
+    $valid = $row["VALID"];
+    if($valid == 1)
+      return true;
+    else
+      return false;
+  }
+  function insertStaff($staff){
+    $prepare = $GLOBALS["connect"]->prepare("INSERT INTO NHANVIEN(HOTENNV,CHUCVU,DIACHI,SODIENTHOAI)VALUES(?,?,?,?)");
+    $name = $staff->getName();
+    $position = $staff->getPosition();
+    $address = $staff->getAddress();
+    $phone = $staff->getPhone();
+    $prepare->bind_param("ssss",$name,$position,$address,$phone);
+    if($prepare->execute()){
+      $prepare->close();
+      $GLOBALS["connect"]->close();
+      return true;
+    }
+    else{
+      $prepare->close();
+      $GLOBALS["connect"]->close();
+      return false;
+    }
+  }
+function updateStaff($staff){
+  $prepare = $GLOBALS["connect"]->prepare(
+    "UPDATE NHANVIEN SET HOTENNV = ?,CHUCVU = ?,DIACHI = ?,SODIENTHOAI = ? WHERE MSNV = ?"
+  );
+  $name = $staff->getName();
+  $position = $staff->getPosition();
+  $address = $staff->getAddress();
+  $phone = $staff->getPhone();
+  $id = $staff->getId();
+  $prepare->bind_param("ssssi",$name,$position,$address,$phone,$id);
+  if($prepare->execute()){
+    $prepare->close();
+    $GLOBALS["connect"]->close();
+    return true;
+  }
+  else{
+    $prepare->close();
+    $GLOBALS["connect"]->close();
+    return false;
+  }
+}
+?>
