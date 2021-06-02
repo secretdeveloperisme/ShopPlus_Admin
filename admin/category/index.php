3484e452d78973cd54a32aa5bedb62615c6af75c
Nguyen Hoang Linh <linh072217@gmail.com>
2021-06-02 11:01:35 +0700

    rename index category

--- admin/category/index.php
+++ admin/category/index.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["id"])){
+        header("Location: /ShopPlus_Admin");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,13 +11,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../../assets/css/base.css">
+  <link rel="stylesheet" href="../../assets/css/base.css">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="../../js/base.js"></script>
+  <script src="../../js/category.js"></script>
   <title>DashBoard</title>
 </head>
 <body>
+  <div id="toast"></div>
   <div class="modal" id="addModal">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -20,10 +31,10 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-          <input type="text" name="" id="" class="form-control">
+          <input type="text" name="" id="inputDataCategory" class="form-control" placeholder="Nhập Tên Loại Hàng">
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-success">Thêm</button>
+          <button type="button" class="btn btn-success" id="btnAddCategory">Thêm</button>
           <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
         </div>
       </div>
@@ -37,7 +48,7 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-          <input type="text" name="" id="" class="form-control">
+          <input type="text" name="" id="" class="form-control" >
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-success">Sửa</button>
@@ -69,7 +80,7 @@
             <button id="btnDeleteCategory" class="btn bg-danger text-white"><i class="fas fa-minus"></i> DELETE</button>
           </div>
         </div>
-        <table class="table table-striped table-hover">
+        <table class="table table-striped table-hover" id="categoryTable">
           <thead>
             <tr>
               <th scope="col">Check</th>
@@ -77,6 +88,7 @@
               <th scope="col">Tên Loại Hàng</th>
               <th scope="col">Edit</th>
             </tr>
+          </thead>
             <tbody>
               <tr>
                 <td scope="row"><input type="checkbox" class="custom-checkbox"></td>
@@ -84,21 +96,7 @@
                 <td>máy tính</td>
                 <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#editModal">edit</button></td>
               </tr>
-              <tr>
-                <td scope="row"><input type="checkbox" class="custom-checkbox"></td>
-                <th>1</th>
-                <td>máy tính</td>
-                <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#editModal">edit</button></td>
-              </tr>
-              <tr>
-                <td scope="row"><input type="checkbox" class="custom-checkbox"></td>
-                <th>1</th>
-                <td>máy tính</td>
-                <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#editModal">edit</button></td>
-              </tr>
-              
-            </tbody>
-          </thead>
+            </tbody
         </table>
       </div>
     </div>
